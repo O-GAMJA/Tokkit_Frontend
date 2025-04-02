bce63a2db8c829dd48911023ae6f9808a457df82
Chamin Im <ckals413@naver.com>
2025-04-02 17:25:12 +0900

    :sparkles: [dev]상태바 제거 및 앱바 제거, 햄버거 버튼 추가

--- app/src/main/java/com/example/tokkit/MainActivity.kt
+++ app/src/main/java/com/example/tokkit/MainActivity.kt
@@ -3,46 +3,39 @@ package com.example.tokkit
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.tokkit.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // 툴바 설정
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        // 상태바 투명하게 설정
+       setStatusBarTransparent()
 
-        // DrawerLayout 설정
-        drawerLayout = findViewById(R.id.drawerLayout)
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 햄버거 메뉴 버튼 클릭 이벤트 설정
+        binding.btnMenu.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
 
         // NavigationView 설정
-        val navigationView = findViewById<NavigationView>(R.id.navigationView)
-        navigationView.setNavigationItemSelectedListener(this)
+        binding.navigationView.setNavigationItemSelectedListener(this)
 
         // 초기 프래그먼트 설정
         if (savedInstanceState == null) {
             replaceFragment(HomeFragment())
-            navigationView.setCheckedItem(R.id.nav_home)
+            binding.navigationView.setCheckedItem(R.id.nav_home)
         }
 
         // 바텀 네비게이션 설정
@@ -52,40 +45,51 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setupSearchFab()
     }
 
+    //상태 바 투명하게
+    private fun setStatusBarTransparent() {
+        window.apply {
+            setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
     }
 
     private fun setupBottomNavigation() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
         // 바텀 네비게이션 배경 제거
-        bottomNavigationView.background = null
+        binding.bottomNavigationView.background = null
 
         // 가운데 아이템 비활성화
-        bottomNavigationView.menu.getItem(2).isEnabled = false
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
-        bottomNavigationView.setOnItemSelectedListener { item ->
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.fragment_home -> {
                     replaceFragment(HomeFragment())
-                    findViewById<NavigationView>(R.id.navigationView).setCheckedItem(R.id.nav_home)
+                    binding.navigationView.setCheckedItem(R.id.nav_home)
                 }
                 R.id.fragment_search -> {
                     replaceFragment(SearchFragment())
-                    findViewById<NavigationView>(R.id.navigationView).setCheckedItem(R.id.nav_search)
+                    binding.navigationView.setCheckedItem(R.id.nav_search)
                 }
                 R.id.fragment_review -> {
                     replaceFragment(ReviewFragment())
-                    findViewById<NavigationView>(R.id.navigationView).setCheckedItem(R.id.nav_review)
+                    binding.navigationView.setCheckedItem(R.id.nav_review)
                 }
                 R.id.fragment_settings -> {
                     replaceFragment(MypageFragment())
-                    findViewById<NavigationView>(R.id.navigationView).setCheckedItem(R.id.nav_mypage)
+                    binding.navigationView.setCheckedItem(R.id.nav_mypage)
                 }
             }
             true
@@ -93,8 +97,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun setupSearchFab() {
-        val searchFab = findViewById<FloatingActionButton>(R.id.searchFab)
-        searchFab.setOnClickListener {
+        binding.searchFab.setOnClickListener {
             // FAB 클릭 시 동작 구현
             val intent = Intent(this, AttachReferenceActivity::class.java)
             startActivity(intent)
@@ -113,19 +116,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (item.itemId) {
             R.id.nav_home -> {
                 replaceFragment(HomeFragment())
-                findViewById<BottomNavigationView>(R.id.bottomNavigationView).selectedItemId = R.id.fragment_home
+                binding.bottomNavigationView.selectedItemId = R.id.fragment_home
             }
             R.id.nav_search -> {
                 replaceFragment(SearchFragment())
-                findViewById<BottomNavigationView>(R.id.bottomNavigationView).selectedItemId = R.id.fragment_search
+                binding.bottomNavigationView.selectedItemId = R.id.fragment_search
             }
             R.id.nav_review -> {
                 replaceFragment(ReviewFragment())
-                findViewById<BottomNavigationView>(R.id.bottomNavigationView).selectedItemId = R.id.fragment_review
+                binding.bottomNavigationView.selectedItemId = R.id.fragment_review
             }
             R.id.nav_mypage -> {
                 replaceFragment(MypageFragment())
-                findViewById<BottomNavigationView>(R.id.bottomNavigationView).selectedItemId = R.id.fragment_settings
+                binding.bottomNavigationView.selectedItemId = R.id.fragment_settings
             }
             R.id.nav_settings -> {
                 // 설정 화면으로 이동하는 코드 (필요시 추가)
@@ -134,7 +137,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 // 로그아웃 기능 구현 (필요시 추가)
             }
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
