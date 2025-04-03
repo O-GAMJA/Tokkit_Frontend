755a97bbdd73a97da993e13640204bcf0788bcda
Chamin Im <ckals413@naver.com>
2025-04-03 21:50:13 +0900

    :sparkles: [#20] article 단계 정보 추가

--- app/src/main/java/com/example/tokkit/model/Article.kt
+++ app/src/main/java/com/example/tokkit/model/Article.kt
@@ -4,5 +4,6 @@ data class Article(
     val title: String,
     val content: String,
     val date: String,
-    val imageResId: Int
+    val imageResId: Int,
+    val stage: Int = 0 // 단계 정보, 0-> 전체 1-5 -> 나머지
 )
