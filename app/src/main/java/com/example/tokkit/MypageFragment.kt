ac31f2ca106b63b438f4537b3a49d46e5af8814a
seungjae708 <choesj365@gmail.com>
2025-04-09 16:44:34 +0900

    :sparkles: [#24] 기능 설정 화면 (FAQ 리사이클러뷰, 어댑터)

--- app/src/main/java/com/example/tokkit/MypageFragment.kt
+++ app/src/main/java/com/example/tokkit/MypageFragment.kt
@@ -27,25 +27,23 @@ class MypageFragment : Fragment() {
 
         // 프로필 수정 클릭
         binding.editProfile.setOnClickListener {
-            Toast.makeText(requireContext(), "프로필 수정 클릭됨", Toast.LENGTH_SHORT).show()
+            // TODO: 프로필 수정 Activity로 이동
         }
 
         // 알림 설정 클릭
         binding.alarmForward.setOnClickListener {
-            Toast.makeText(requireContext(), "알림 설정 이동", Toast.LENGTH_SHORT).show()
             val intent = Intent(requireContext(), AlarmSettingActivity::class.java)
             startActivity(intent)
         }
 
         // 기능 설정 클릭
         binding.functionForward.setOnClickListener {
-            Toast.makeText(requireContext(), "기능 설정 이동", Toast.LENGTH_SHORT).show()
-            // TODO: 기능 설정 Activity로 이동
+            val intent = Intent(requireContext(), FunctionSettingActivity::class.java)
+            startActivity(intent)
         }
 
         // 로그아웃 클릭
         binding.logout.setOnClickListener {
-            Toast.makeText(requireContext(), "로그아웃 클릭됨", Toast.LENGTH_SHORT).show()
             // TODO: 로그아웃 처리
         }
     }
