9cec8a6aa36007e12d6775b0e061dac59c1cdf59
goeka <goeka1337@gmail.com>
2025-05-27 23:08:03 +0900

    ✨ [#61] 알림 title을 백에서 보내준 title로 설정하도록 수정, 푸시알림에 앱로고 삽입. v2

--- app/src/main/java/com/example/tokkit/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/tokkit/service/MyFirebaseMessagingService.kt
@@ -5,10 +5,12 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.tokkit.MainActivity // 알림 클릭 시 열 앱 액티비티
 import com.example.tokkit.R
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -22,20 +24,24 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // 데이터 메시지 확인
         if (remoteMessage.data.isNotEmpty()) {
             Log.d("FCM", "Message data payload: ${remoteMessage.data}")
+            val title = remoteMessage.data["title"] ?: "Tokkit 알림"
             val body = remoteMessage.data["body"] ?: "데이터 메시지가 도착했습니다"
-            sendNotification(body)
+            sendNotification(title, body)
         }
 
         // 알림 메시지 확인
         remoteMessage.notification?.let {
-            Log.d("FCM", "Message Notification Body: ${it.body}")
-            sendNotification(it.body ?: "알림 메시지가 도착했습니다")
+            val title = it.title ?: "Tokkit 알림"
+            val body = it.body ?: "알림 메시지가 도착했습니다"
+            sendNotification(title, body)
         }
+
     }
 
-    private fun sendNotification(messageBody: String) {
+    private fun sendNotification(title: String, messageBody: String) {
         val channelId = "default_channel_id"
         val notificationId = 1001
+        val largeIcon = BitmapFactory.decodeResource(resources, R.drawable.rabbit1)
 
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
@@ -47,13 +53,15 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         )
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            //.setSmallIcon(R.drawable.ic_stat_ic_notification)
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setContentTitle("Tokkit 알림")
+            .setSmallIcon(R.drawable.rabbit_icon) // 흰색 벡터가 더 권장됨
+            .setContentTitle(title)
             .setContentText(messageBody)
+            .setLargeIcon(largeIcon) // ✅ 오른쪽 상단에 원형으로 표시됨
             .setAutoCancel(true)
-            .setColor(Color.BLUE)
+            //.setColor(Color.GRAY)
+            .setColor(ContextCompat.getColor(this, R.color.bora200))
             .setContentIntent(pendingIntent)
+
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
